b1aa481dee39430089f9525a006008ad6aab9a2a
Andres-Salamanca <andrealebarbaritos@gmail.com>
2023-09-23 21:31:13 -0500

    ya esta la parte de añadir mascotas sin pasar la informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MascotaCrudComponent } from './Mascota/mascota-crud/mascota-crud.component';
+import { CrearMascotaComponent } from './Mascota/crear-mascota/crear-mascota.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MascotaCrudComponent
+    MascotaCrudComponent,
+    CrearMascotaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
