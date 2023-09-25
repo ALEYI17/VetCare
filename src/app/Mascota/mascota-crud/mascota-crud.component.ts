1f4a273b4c5b81ee9f030399d4aff9b02bcf1f3c
ALEYI17 <aleelaleyi@gmail.com>
2023-09-25 11:16:02 -0500

    Arregle la searchbar y los botones de ir de abajo a arriba

--- src/app/Mascota/mascota-crud/mascota-crud.component.ts
+++ src/app/Mascota/mascota-crud/mascota-crud.component.ts
@@ -7,44 +7,80 @@ import { MascotaService } from 'src/app/service/mascota.service';
   styleUrls: ['./mascota-crud.component.css']
 })
 export class MascotaCrudComponent {
-    selectedMascota!: Mascota;
-    mostrarforman:boolean = false;
-    mostrarforman2:boolean = false;
-    
-    listaDeMascotas!: Mascota[] ;
-    
+  selectedMascota!: Mascota;
+  mostrarforman: boolean = false;
+  mostrarforman2: boolean = false;
 
-    constructor(private mascotaServicio: MascotaService){
-        
-    }
-
-    mostrarMascota(mascota: Mascota){
-      this.selectedMascota = mascota;
-      this.mostrarforman2 = true
-    }
-    
-    onMascotaAdded(newMascota: Mascota) {
-      // Push the new pet to the array
-      this.listaDeMascotas.push(newMascota);
-      console.log('Mascota added to listaDeMascotas:', newMascota);
-    }
-    actualizarMascota(updateMascota: Mascota){
-      const id = updateMascota.ID -1
-      this.listaDeMascotas[id] = updateMascota
-      
-    }
-
-    eliminarMascota(mascota:Mascota){
-      var index = this.listaDeMascotas.indexOf(mascota)
-      this.listaDeMascotas.splice(index,1)
-    }
-    
-    mostararAnadir(){
-      this.mostrarforman = true
-    }
-    ngOnInit():void{
-        this.listaDeMascotas = this.mascotaServicio.findAll();
-    }
+  listaDeMascotas!: Mascota[];
 
 
+  constructor(private mascotaServicio: MascotaService) {
+
+  }
+
+  mostrarMascota(mascota: Mascota) {
+    this.selectedMascota = mascota;
+    this.mostrarforman2 = true
+  }
+
+  onMascotaAdded(newMascota: Mascota) {
+    // Push the new pet to the array
+    this.listaDeMascotas.push(newMascota);
+    console.log('Mascota added to listaDeMascotas:', newMascota);
+  }
+  actualizarMascota(updateMascota: Mascota) {
+    const id = updateMascota.ID - 1
+    this.listaDeMascotas[id] = updateMascota
+
+  }
+
+  eliminarMascota(mascota: Mascota) {
+    var index = this.listaDeMascotas.indexOf(mascota)
+    this.listaDeMascotas.splice(index, 1)
+  }
+
+  mostararAnadir() {
+    this.mostrarforman = true
+  }
+  ngOnInit(): void {
+    this.listaDeMascotas = this.mascotaServicio.findAll();
+    const searchInput = document.getElementById('searchInput') as HTMLInputElement;
+    const clientTable = document.getElementById('clientTable');
+
+    if (searchInput && clientTable) {
+      const rows = clientTable.getElementsByTagName('tbody')[0]?.getElementsByTagName('tr');
+
+      if (rows) {
+        searchInput.addEventListener('input', function () {
+          const searchText = searchInput.value.toLowerCase();
+
+          for (let i = 0; i < rows.length; i++) {
+            const clientRow = rows[i];
+            const clientName = clientRow.getElementsByTagName('th')[0]?.textContent?.toLowerCase();
+
+            if (clientName && clientName.includes(searchText)) {
+              clientRow.style.display = '';
+            } 
+            else {
+              clientRow.style.display = 'none';
+            }
+          }
+        });
+      }
     }
+  }
+  scrollToBottom() {
+    const element = document.getElementById('abajo');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  scrollToTop() {
+    const element = document.getElementById('arriba');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+}
+
